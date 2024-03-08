35bc2abf795c18215e90afba945f38f41f9de5c7
misaki <mi3mi3king@gmail.com>
2024-03-08 13:14:54 +0900

    スの調整の

--- src/entry/choice.php
+++ src/entry/choice.php
@@ -5,7 +5,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../assets/css/entry.css">
+    <link rel="stylesheet" href="../assets/css/choice.css">
     <title>choice</title>
     <link
       rel="stylesheet"
