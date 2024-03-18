3d4d01199e673ce4119b12aefd256152bbd99dd6
shota0827 <posse22sho@gmail.com>
2024-03-18 18:01:01 +0900

    共通ヘッダー３を作りました

---
+++ src/includes/header3.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../assets/css/reset.css">
+    <link rel="stylesheet" href="../assets/css/includes.css">
+</head>
+<body>
+<header>
+        <div class="header_parent">
+            <div>
+                <a href="/"><img src="../assets/img/header_logo.png" alt="CRAFT" class="header_logo"></a>
+            </div>
+            <div>
+                <a href="/"><img src="../assets/img/boozer_logo-black.png" alt="boozer inc" class="header_logo_boozer"></a>
+            </div>
+        </div>
+    </header>
+</body>
+</html>
