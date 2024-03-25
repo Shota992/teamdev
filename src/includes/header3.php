75bea63271b91534476fa9bc543d95f2a1659c22
Ouka <negi0141017323@gmail.com>
2024-03-25 16:51:48 +0900

    ログインページ・ヘッダー3、フッター2の修正

--- src/includes/header3.php
+++ src/includes/header3.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,16 +8,18 @@
     <link rel="stylesheet" href="../assets/css/reset.css">
     <link rel="stylesheet" href="../assets/css/includes.css">
 </head>
+
 <body>
-<header>
+    <header>
         <div class="header_parent">
             <div>
                 <a href="/"><img src="../assets/img/header_logo.png" alt="CRAFT" class="header_logo"></a>
             </div>
-            <div>
+            <div class="header_logo_boozerImg">
                 <a href="/"><img src="../assets/img/boozer_logo-black.png" alt="boozer inc" class="header_logo_boozer"></a>
             </div>
         </div>
     </header>
 </body>
+
 </html>
