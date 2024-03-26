4335371e5bbb3b307ae64fbfd5ab67391a026185
yanagita1007 <maachan1510@gmail.com>
2024-03-26 12:51:39 +0900

    前ページにheaderとfooterつけたつもり

--- src/Eadmin/student.php
+++ src/Eadmin/student.php
@@ -27,65 +27,79 @@ $agents = $stmt->fetchAll(PDO::FETCH_ASSOC);
     <script src="./assets/js/script.js">
     </script>
 </head>
+
 <body class="body-c">
-    <div class="student-wrapper">
-        <div class="side-container">
-            <nav class="index-side-contents">
-            <a href="#" class="side-content side-choiced">
-                <div class="side-sent side-choiced">学生情報一覧</div>
-            </a>
-            <a href="./password.php" class="side-content">
-                <div class="side-sent">パスワード変更
-                </div>
-            </a>
-            <a href="./../top/top.php" class="side-content">
-                <div class="side-sent">ログアウト</div>
-            </a>
-            </nav>
-        </div>
-        <div class="student-main-container">
-            <div class="student-main-inner">
-                <div class="student-main-head">
-                    <div class="student-main-head-container">
-                        <div class="student-main-head-sent">エージェント企業一覧</div>
+    <header class="header">
+        <a href="" class="header-logo" target="_blank" rel="noopener noreferrer">
+            <img src="../assets/img/header_logo.png" alt="CRAFT">
+        </a>
+        <img src="../assets/img/boozer_logo-black.png" alt="boozer">
+    </header>
+    <main>
+        <div class="student-wrapper">
+            <div class="side-container">
+                <nav class="index-side-contents">
+                <a href="#" class="side-content side-choiced">
+                    <div class="side-sent side-choiced">学生情報一覧</div>
+                </a>
+                <a href="./password.php" class="side-content">
+                    <div class="side-sent">パスワード変更
                     </div>
-                </div>
-                <div class="student-main-name">
-                    <div class="student-main-name-title">○○企業様</div>
-                    <div class="student-main-name-line"></div>
-                </div>
-                <div class="student-main-table">
-                    <table  class="student-main-table-container">
-                        <tr class="student-main-table-head">
-                            <td class="student-main-table-content">学生名</td>
-                            <td class="student-main-table-content">フリガナ</td>
-                            <td class="student-main-table-content">性別</td>
-                            <td class="student-main-table-content">大学名</td>
-                            <td class="student-main-table-content">携帯電話番号</td>
-                            <td class="student-main-table-content">メールアドレス</td>
-                            <td class="student-main-table-content">卒業年度</td>
-                            <td class="student-main-table-content">文理区分</td>
-                            <td class="student-main-table-content">志望業界</td>
-                            <td class="student-main-table-content">申込み日時</td>
-                        </tr>
-                        <?php foreach ($agents as $student) { ?>
-                            <tr class="student-main-table-contents student-odd">
-                                <td class="student-main-table-content"><?=$student["name"];?></td>
-                                <td class="student-main-table-content"><?=$student["sub_name"];?></td>
-                                <td class="student-main-table-content"><?=$student["sex"];?></td>
-                                <td class="student-main-table-content"><?=$student["school"];?></td>
-                                <td class="student-main-table-content"><?=$student["tel_num"];?></td>
-                                <td class="student-main-table-content"><?=$student["mail"];?></td>
-                                <td class="student-main-table-content"><?=$student["graduation"];?></td>
-                                <td class="student-main-table-content"><?=$student["division"];?></td>
-                                <td class="student-main-table-content"><?=$student["desire"];?></td>
-                                <td class="student-main-table-content">20204/03/12 21:45</td>
+                </a>
+                <a href="./../top/top.php" class="side-content">
+                    <div class="side-sent">ログアウト</div>
+                </a>
+                </nav>
+            </div>
+            <div class="student-main-container">
+                <div class="student-main-inner">
+                    <div class="student-main-head">
+                        <div class="student-main-head-container">
+                            <div class="student-main-head-sent">エージェント企業一覧</div>
+                        </div>
+                    </div>
+                    <div class="student-main-name">
+                        <div class="student-main-name-title">○○企業様</div>
+                        <div class="student-main-name-line"></div>
+                    </div>
+                    <div class="student-main-table">
+                        <table  class="student-main-table-container">
+                            <tr class="student-main-table-head">
+                                <td class="student-main-table-content">学生名</td>
+                                <td class="student-main-table-content">フリガナ</td>
+                                <td class="student-main-table-content">性別</td>
+                                <td class="student-main-table-content">大学名</td>
+                                <td class="student-main-table-content">携帯電話番号</td>
+                                <td class="student-main-table-content">メールアドレス</td>
+                                <td class="student-main-table-content">卒業年度</td>
+                                <td class="student-main-table-content">文理区分</td>
+                                <td class="student-main-table-content">志望業界</td>
+                                <td class="student-main-table-content">申込み日時</td>
                             </tr>
-                        <?php } ?>
-                    </table>
+                            <?php foreach ($agents as $student) { ?>
+                                <tr class="student-main-table-contents student-odd">
+                                    <td class="student-main-table-content"><?=$student["name"];?></td>
+                                    <td class="student-main-table-content"><?=$student["sub_name"];?></td>
+                                    <td class="student-main-table-content"><?=$student["sex"];?></td>
+                                    <td class="student-main-table-content"><?=$student["school"];?></td>
+                                    <td class="student-main-table-content"><?=$student["tel_num"];?></td>
+                                    <td class="student-main-table-content"><?=$student["mail"];?></td>
+                                    <td class="student-main-table-content"><?=$student["graduation"];?></td>
+                                    <td class="student-main-table-content"><?=$student["division"];?></td>
+                                    <td class="student-main-table-content"><?=$student["desire"];?></td>
+                                    <td class="student-main-table-content">20204/03/12 21:45</td>
+                                </tr>
+                            <?php } ?>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
+    </main>
+    <footer class="footer">
+        <div class="footer-copyright">
+            <small class="copyright">&copy; POSSE,Inc</small>
+        </div>
+    </footer>
 </body>
 </html>
