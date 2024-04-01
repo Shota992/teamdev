9426af46930265508066daeef3a212c6fb0ef25c
yanagita1007 <maachan1510@gmail.com>
2024-04-02 00:08:43 +0900

    サイドバー編集

--- src/Cadmin/index.php
+++ src/Cadmin/index.php
@@ -75,78 +75,74 @@ $dbh = null;
         <aside class="side-container">
             <nav>
                 <div class="side-sent">
-                    <div class="side-content"><a href="/">エージェント企業一覧</a></div>
+                    <div class="side-choiced"><a href="#">エージェント企業一覧</a></div>
                     <div class="side-content"><a href="../Cadmin/egent/create.php">エージェント企業新規登録</a></div>
-                    <div class="side-content"><a href="../Cadmin/auth/newadmin.php"></a>新規管理者登録</a></div>
-                    <div class="side-choiced"><a href="../Cadmin/content.php">申込内容一覧</a></div>
+                    <div class="side-content"><a href="../Cadmin/auth/newadmin.php">新規管理者登録</a></div>
+                    <div class="side-content"><a href="../Cadmin/content.php">申込内容一覧</a></div>
                     <div class="side-content"><a href="../Cadmin/auth/logout.php">ログアウト</a></div>
                 </div>
             </nav>
         </aside>
         <main class="create-main">
-            <!-- <div class="index-main-container"> -->
-                <!-- <div class="index-main-inner"> -->
-                    <div class="index-main-head">
-                        <div class="index-main-head-container">
-                            <div class="index-main-head-sent">エージェント企業一覧</div>
+            <div class="index-main-head">
+                <div class="index-main-head-container">
+                    <div class="index-main-head-sent">エージェント企業一覧</div>
+                </div>
+            </div>
+            <form method="GET" action="./index.php">
+                <div class="index-main-search">
+                    <div class="index-main-search-contents">
+                        <div class="index-main-search-content">
+                            <div class="index-main-search-title">サービス名</div>
+                            <input class="index-main-search-input" type="text" placeholder="検索" autocomplete="off" name="search-site" value="<?php if (!empty($_GET['search_site'])) {
+                                                                                                                                                    echo $_GET['search_site'];
+                                                                                                                                                } ?>">
                         </div>
-                    </div>
-                    <form method="GET" action="./index.php">
-                        <div class="index-main-search">
-                            <div class="index-main-search-contents">
-                                <div class="index-main-search-content">
-                                    <div class="index-main-search-title">サービス名</div>
-                                    <input class="index-main-search-input" type="text" placeholder="検索" autocomplete="off" name="search-site" value="<?php if (!empty($_GET['search_site'])) {
-                                                                                                                                                            echo $_GET['search_site'];
-                                                                                                                                                        } ?>">
-                                </div>
-                                <div class="index-main-search-content">
-                                    <div class="index-main-search-title">企業名</div>
-                                    <input class="index-main-search-input" type="text" placeholder="検索" autocomplete="off" name="search-agent" value="<?php if (!empty($_GET['search_agent'])) {
-                                                                                                                                                            echo $_GET['search_agent'];
-                                                                                                                                                        } ?>">
-                                </div>
-                                <button class="index-main-search-button">
-                                    <input type="submit" name="search" value="検索">
-                                </button>
-                            </div>
+                        <div class="index-main-search-content">
+                            <div class="index-main-search-title">企業名</div>
+                            <input class="index-main-search-input" type="text" placeholder="検索" autocomplete="off" name="search-agent" value="<?php if (!empty($_GET['search_agent'])) {
+                                                                                                                                                    echo $_GET['search_agent'];
+                                                                                                                                                } ?>">
                         </div>
-                    </form>
-                    <div class="index-main-table">
-                        <table class="index-main-table-conainer">
-                            <tr class="index-main-table-head">
-                                <td class="index-main-table-content">企業ID</td>
-                                <td class="index-main-table-content">サービス名</td>
-                                <td class="index-main-table-content">企業名</td>
-                                <td class="index-main-table-content">メールアドレス</td>
-                            </tr>
-                            <?php foreach ($infos as $info) { ?>
-                                <tr class="index-main-table-contents index-odd" id="infos-<?= $info["id"] ?>">
-                                    <td class="index-main-table-content">
-                                        <a href="./egent/edit.php?id=<?= $info["id"] ?>">
-                                            <?= $info["agent_id"]; ?>
-                                        </a>
-                                    </td>
-                                    <td class="index-main-table-content">
-                                        <a href="./egent/edit.php?id=<?= $info["id"] ?>">
-                                            <?= $info["site_name"]; ?>
-                                        </a>
-                                    </td>
-                                    <td class="index-main-table-content">
-                                        <a href="./egent/edit.php?id=<?= $info["id"] ?>">
-                                            <?= $info["agent_name"]; ?>
-                                        </a>
-                                    </td>
-                                    <td class="index-main-table-content">
-                                        <a href="./egent/edit.php?id=<?= $info["id"] ?>">
-                                            <?= $info["email"]; ?>
-                                        </a>
-                                    </td>
-                                </tr>
-                            <?php } ?>
-                        </table>
+                        <button class="index-main-search-button">
+                            <input type="submit" name="search" value="検索">
+                        </button>
                     </div>
                 </div>
+            </form>
+            <div class="index-main-table">
+                <table class="index-main-table-conainer">
+                    <tr class="index-main-table-head">
+                        <td class="index-main-table-content">企業ID</td>
+                        <td class="index-main-table-content">サービス名</td>
+                        <td class="index-main-table-content">企業名</td>
+                        <td class="index-main-table-content">メールアドレス</td>
+                    </tr>
+                    <?php foreach ($infos as $info) { ?>
+                        <tr class="index-main-table-contents index-odd" id="infos-<?= $info["id"] ?>">
+                            <td class="index-main-table-content">
+                                <a href="./egent/edit.php?id=<?= $info["id"] ?>">
+                                    <?= $info["agent_id"]; ?>
+                                </a>
+                            </td>
+                            <td class="index-main-table-content">
+                                <a href="./egent/edit.php?id=<?= $info["id"] ?>">
+                                    <?= $info["site_name"]; ?>
+                                </a>
+                            </td>
+                            <td class="index-main-table-content">
+                                <a href="./egent/edit.php?id=<?= $info["id"] ?>">
+                                    <?= $info["agent_name"]; ?>
+                                </a>
+                            </td>
+                            <td class="index-main-table-content">
+                                <a href="./egent/edit.php?id=<?= $info["id"] ?>">
+                                    <?= $info["email"]; ?>
+                                </a>
+                            </td>
+                        </tr>
+                    <?php } ?>
+                </table>
             </div>
         </main>
     </div>
